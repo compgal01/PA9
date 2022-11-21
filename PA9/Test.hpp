286b859e88afbcde678bd4c37780bcd759cdb8d4
TheCongaGuy <drew.evensen@gmail.com>
2022-11-20 17:43:36 -0800

    Create Dedicated Test Header File
    Moved code from main to it's own dedicated method in a Test.hpp file. Additionally, added regions to the header file to separate sections of code.

--- PA9/Test.hpp
+++ PA9/Test.hpp
@@ -1,17 +1,22 @@
+#pragma once
+
 /*******************************************************************************
  * Programmers: Drew Evensen		                                           *
  * Class: CptS 122				                                               *
  * Programming Assignment: 9	                                               *
  * Date: 12/07/22                                                              *
  *                                                                             *
- * Description:																   *
+ * Description:	File consisting of test methods. Not part of final solution.   *
+ *				Used for experimenting and ensuring program works.			   *
  *******************************************************************************/
 
-#include "Test.hpp"
+#include <SFML/Graphics.hpp>// External Library
+#include <iostream>
+#include <random>
+#include <Windows.h>
+#include <time.h>
 
-int main(int argc, char argv[])
-{
-	graphicsTest();
-
-	return 0;
-}
+// Test function that demonstrates a visualization of selection sort for the purposes of checking and understanding 
+// the graphics library
+// Programmer: Drew Evensen
+void graphicsTest();
---
+++ PA9/test.cpp
@@ -0,0 +1,116 @@
+#include "Test.hpp"
+
+/*******************************************************************************
+ * Programmers: Drew Evensen		                                           *
+ * Class: CptS 122				                                               *
+ * Programming Assignment: 9	                                               *
+ * Date: 12/07/22                                                              *
+ *                                                                             *
+ * Description:	File consisting of test methods. Not part of final solution.   *
+ *				Used for experimenting and ensuring program works.			   *
+ *******************************************************************************/
+
+ // Test function that demonstrates a visualization of selection sort for the purposes of checking and understanding 
+ // the graphics library
+ // Programmer: Drew Evensen
+void graphicsTest()
+{
+#pragma region Pre-Window Setup
+	// Test array
+	int test[30];
+
+	// Iterator for selection sort
+	int b = 0, bMin = 0;
+
+	// Set a new seed for rand()
+	srand((unsigned)time(NULL));
+
+	// Create an array of 30 random integers
+	for (int i = 0; i < 30; i++)
+		test[i] = rand() % 500 + 1;
+
+	// Display the starting array
+	std::cout << "Test Array Initialized. Starting Config:" << std::endl;
+	for (int i = 0; i < 30; i++) std::cout << test[i] << ", ";
+
+	// Array of SFML rectangle objects to print to the screen
+	sf::RectangleShape items[30];
+
+	// Create new test window       (Size of Window)               (Title of Window)
+	sf::RenderWindow testWindow(sf::VideoMode(1200, 500), "Selection Sort Algorithm Example");
+#pragma endregion
+
+#pragma region Window Open
+	// Run while the window is open
+	while (testWindow.isOpen())
+	{
+	#pragma region Event Manager
+		// Catches an event in the window
+		sf::Event event;
+
+		// Close the window upon clicking the X
+		while (testWindow.pollEvent(event))
+
+			if (event.type == 0) // Equivalent to if statement below
+		 // if (event.type == sf::Event::Closed)
+		 // sf::Event::Closed is an enum with position 0, I.E. sf::Event::Closed == 0
+
+				testWindow.close();
+	#pragma endregion
+
+	#pragma region Selection Sort
+		// Iterate a step through selection sort
+		if (b < 30)
+		{
+			bMin = b;
+			for (int i = b; i < 30; i++)
+				if (test[i] < test[bMin])
+					bMin = i;
+
+			int temp = test[b];
+			test[b] = test[bMin];
+			test[bMin] = temp;
+
+			b++;
+		}
+	#pragma endregion
+
+	#pragma region Graphics Manager
+		// Create each rectangle item
+		for (int i = 0; i < 30; i++)
+		{
+			// SFML Rectangle takes a SFML Vector 2, meaning a pair of 2 floating point numbers
+			items[i] = sf::RectangleShape(sf::Vector2f(40.f, (float)test[i]));
+			// SFML Rectangle member function setFillColor takes a static constant Color for the requested color
+			// Can find available colors under the sf::Color class
+			items[i].setFillColor(sf::Color::White);
+			items[i].setOrigin(0.f, test[i] - 500.f);
+
+			// Mark each changed rectangle red
+			if (b == 30)
+				items[i].setFillColor(sf::Color::Green);
+
+			// Mark each rectangle green when sorted
+			else if (bMin == i || b == i)
+				items[i].setFillColor(sf::Color::Red);
+		}
+
+		// Clear the window
+		testWindow.clear();
+
+		// Draw the items to the screen
+		for (int i = 0; i < 30; i++)
+		{
+			// Shift each rectangle to the correct position before drawing
+			items[i].move(i * 40.f, 0.f);
+			testWindow.draw(items[i]);
+		}
+
+		// Draw the window
+		testWindow.display();
+
+		Sleep(100);
+	#pragma endregion
+	}
+#pragma endregion
+}
